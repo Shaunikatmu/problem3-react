21bb7280e3ebf05c9e754695097351495f01fe68
Shaunikatmu <shaunika.shokeen.ss@gmail.com>
2024-12-05 20:44:30 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Webpage for Problem 3
+
+This React application was built to demonstrate the use of React and deployment on Vercel.
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to React Webpage</h1>
+                <p>This page was built using React and hosted on Vercel.</p>
+                <h2>Installation Process</h2>
+                <p>
+                    React was set up using <code>npx create-react-app</code>. I developed the app
+                    locally and then deployed it to Vercel.
+                </p>
+                <h2>Challenges and Solutions</h2>
+                <p>
+                    I faced some deployment challenges, but I solved them by reading the Vercel
+                    documentation.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
